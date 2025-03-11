b6612def49e35fa0d2fbeafa97dfc0f4d3287910
Stephan Vedder <vedder@mbits.info>
2025-03-11 16:56:51 +0100

    Compat: Integrate GLI through VCPKG

--- GeneralsMD/Code/Libraries/Source/WWVegas/WWLib/always.h
+++ GeneralsMD/Code/Libraries/Source/WWVegas/WWLib/always.h
@@ -42,11 +42,9 @@
 
 #include <assert.h>
 
+#include <new>
 #ifndef _WIN32
 #include "windows_compat.h"
-#else
-// New needs to be included first to avoid collision with headers when building with Clang
-#include <new>
 #endif
 
 // Disable warning about exception handling not being enabled. It's used as part of STL - in a part of STL we don't use.
